9e35862c945c56beefaf24bc8eb71393ad93d606
Oluwafemi <oluwafemiadedeji143@gmail.com>
2026-03-16 21:51:13 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,11 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  server: {
-    allowedHosts: ["unwatery-unbankably-joe.ngrok-free.dev"],
   },
 });
