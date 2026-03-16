fa2982418733f2ba64a950ab2572f6581749207b
Oluwafemi <oluwafemiadedeji143@gmail.com>
2026-03-16 22:07:07 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
   },
 });
